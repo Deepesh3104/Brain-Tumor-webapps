12d379096f0a7364243c66094d296a1e9a06cc8e
Deepesh kumar <deepesh.kumar@ssipmt.com>
2025-03-18 22:05:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -337,5 +337,5 @@ def helpsupot():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000
     app.run(host="0.0.0.0", port=port, debug=True)
